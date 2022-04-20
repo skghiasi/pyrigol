3e53b0b66d542805e112f46a354a2777923de0b7
kghiasi <s.k.ghiasi@tudelft.nl>
2022-04-20 08:48:05 +0200

    added url to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
    name='pyrigol',
-   version='2.0',
+   version='0.0',
    description='Something different',
    author='kghiasi',
    author_email='skyghiassi@gmail.com',
+   url = 'git@gitlab.ewi.tudelft.nl/kghiasi/pyrigol.git'
    packages=['pyrigol'],  # would be the same as name
    install_requires=['pyvisa'], #external packages acting as dependencies
 )
