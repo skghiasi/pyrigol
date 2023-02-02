89d86afc7feee26dd7933faee98b18e52e91ca7c
kghiasi <s.k.ghiasi@tudelft.nl>
2023-02-02 13:22:08 +0100

    reordering the library

---
+++ DG4062/Examples/USB_arbitrary_wave_points_1.py
@@ -0,0 +1,12 @@
+from pathlib import PurePath
+import pathlib
+import sys
+import time
+import pyvisa
+
+
+import lib.definitions as rigol_defs
+import lib.commands as scpi 
+import lib.SCPI_util
+
+
--- pyrigol/DG4062/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-import sys
-print("2.In module products __sys__ , __package__, __name__ ==",sys.path[0], __package__, __name__)
-print(os.getcwd())
